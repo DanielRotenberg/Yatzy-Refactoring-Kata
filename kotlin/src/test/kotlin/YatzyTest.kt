f1739701bcc8682395d004c795a92501fcf53312
danielrotenberg <danirg10000@gmail.com>
2026-05-26 14:44:13 +0300

    * Refactored functions and variables naming inconsistency - now all names are camel case.
    * Refactored the functions to more clean and expressive code, no more imperative loops. now all code uses collections functions transformations, which also leads to removed duplication.

--- kotlin/src/test/kotlin/YatzyTest.kt
+++ kotlin/src/test/kotlin/YatzyTest.kt
@@ -64,33 +64,33 @@ class YatzyTest {
 
     @Test
     fun one_pair() {
-        assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6))
-        assertEquals(6, Yatzy.score_pair(1, 3, 3, 3, 1))
-        assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5))
-        assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5))
-        assertEquals(0, Yatzy.score_pair(3, 4, 1, 5, 6))
+        assertEquals(6, Yatzy.scorePair(3, 4, 3, 5, 6))
+        assertEquals(6, Yatzy.scorePair(1, 3, 3, 3, 1))
+        assertEquals(10, Yatzy.scorePair(5, 3, 3, 3, 5))
+        assertEquals(12, Yatzy.scorePair(5, 3, 6, 6, 5))
+        assertEquals(0, Yatzy.scorePair(3, 4, 1, 5, 6))
     }
 
     @Test
     fun two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5))
-        assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5))
-        assertEquals(0, Yatzy.two_pair(3, 3, 2, 4, 5))
+        assertEquals(16, Yatzy.twoPair(3, 3, 5, 4, 5))
+        assertEquals(16, Yatzy.twoPair(3, 3, 5, 5, 5))
+        assertEquals(0, Yatzy.twoPair(3, 3, 2, 4, 5))
     }
 
     @Test
     fun three_of_a_kind() {
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5))
-        assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5))
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5))
-        assertEquals(0, Yatzy.three_of_a_kind(3, 1, 3, 4, 5))
+        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 4, 5))
+        assertEquals(15, Yatzy.threeOfAKind(5, 3, 5, 4, 5))
+        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 3, 5))
+        assertEquals(0, Yatzy.threeOfAKind(3, 1, 3, 4, 5))
     }
 
     @Test
     fun four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5))
-        assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5))
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3))
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 5))
+        assertEquals(20, Yatzy.fourOfAKind(5, 5, 5, 4, 5))
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 3))
     }
 
     @Test
@@ -103,6 +103,7 @@ class YatzyTest {
     @Test
     fun largeStraight() {
         assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5))
+        assertEquals(20, Yatzy.largeStraight(3, 2, 6, 4, 5))
         assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6))
         assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5))
     }
